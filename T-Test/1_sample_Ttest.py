579046f9fe5b1b4eee78162088c45f523abedaba
pingbell <60215958+pingbell@users.noreply.github.com>
2020-03-01 17:56:32 +0530

    altered

--- T-Test/1_sample_Ttest.py
+++ T-Test/1_sample_Ttest.py
@@ -1,20 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Mar  1 17:24:32 2020
-
+Created on Sun Mar  1 17:24:32 2018
 @author: Shaurya Prakash
 """
 
-from scipy import stats 
+from scipy import stats
 
 # given data 
 mean = 12
 sample =[10,12,20,21,24,18,8]
-std_deviation = "unknown"
+std_deviation = ""
 
 #since sample is small n<30 ,degree of freedom = n-1=6
 print (stats.ttest_1samp(sample,mean))
 
 statistics,pvalue=(stats.ttest_1samp(sample,mean))
 
-print(pvalue)
+print("pvalue="+str(pvalue))
+print("pvalue="+str(pvalue))
