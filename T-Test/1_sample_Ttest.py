4924fa8c16d32a7a49faef40f00b9d0eda92f500
pingbell <60215958+pingbell@users.noreply.github.com>
2020-03-01 17:32:06 +0530

    1-sample t-test basic for unknown standard deviation.

---
+++ T-Test/1_sample_Ttest.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  1 17:24:32 2020
+
+@author: HP
+"""
+
+from scipy import stats 
+
+# given data 
+mean = 12
+sample =[10,12,20,21,24,18,8]
+std_deviation = "unknown"
+
+#since sample is small n<30 ,degree of freedom = n-1=6
+print (stats.ttest_1samp(sample,mean))
+
+statistics,pvalue=(stats.ttest_1samp(sample,mean))
+
+print(pvalue)
